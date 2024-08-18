eb115121bc47459a3e39eea522167969fb1be3fa
Aya Alsakkaf <aya.alsakkaf@ccse.ku.edu.kw>
2024-08-18 19:48:34 +0300

    react day 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import "./assets/css/starter.css";
+import bookstore from "./assets/media/bookstore.jpg";
+import books from "./assets/data/books";
 function App() {
+  let bookStoreName = "Full Stack's Book Store";
+  const displayHPBooks = books.map((book) => {
+    return (
+      <div className="book-container">
+        <img src={book.image} alt="HP book 1" />
+        <h3>{book.title}</h3>
+        <p>{book.price}</p>
+      </div>
+    );
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Welcome to {bookStoreName}</h1>
+      <img src={bookstore} alt="Book store" className="bookstore-image" />
+
+      <div className="book-section">{displayHPBooks}</div>
     </div>
   );
 }
